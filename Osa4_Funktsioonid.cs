9c80555f26599198a01f0f3a8d79d019c342a340
opilane <opilane@HP-CZC2349HTQ>
2026-03-04 13:41:16 +0200

    Add project files.

---
+++ Osa4_Funktsioonid.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public static class Osa4_Funktsioonid
+{
+    // Ülesanne 1
+    public static void SalvestaLemmikToit()
+    {
+        try
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ("C:\\Users\\opilane\\Downloads\\Retseptid.txt"));
+            StreamWriter sw = new StreamWriter(path, true);
+
+            Console.Write("Sisesta üks Itaalia toit: ");
+            string toit = Console.ReadLine();
+
+            sw.WriteLine(toit);
+            sw.Close();
+
+            Console.WriteLine("Toit salvestatud!\n");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Viga faili kirjutamisel!\n");
+        }
+    }
+
+    // Ülesanne 2
+    public static void KuvaMenuu()
+    {
+        try
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "C:\\Users\\opilane\\Downloads\\Retseptid.txt");
+            StreamReader sr = new StreamReader(path);
+            string sisu = sr.ReadToEnd();
+            sr.Close();
+
+            Console.WriteLine("\n--- MENÜÜ ---");
+            Console.WriteLine(sisu);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Viga faili lugemisel või faili ei eksisteeri!\n");
+        }
+    }
+
+    // Ülesanded 3–5
+    public static void KoostisosadeHaldus()
+    {
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "C:\\Users\\opilane\\Downloads\\Koostisosad.txt");
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllLines(path, new string[] { "Tomat", "Juust", "Ketšup" });
+        }
+
+        List<string> list = new List<string>();
+
+        try
+        {
+            foreach (string rida in File.ReadAllLines(path))
+            {
+                list.Add(rida);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Viga faili lugemisel!");
+            return;
+        }
+
+        // Muudame esimese elemendi
+        if (list.Count > 0)
+            list[0] = "Kvaliteetne oliiviõli";
+
+        // Eemaldame Ketšupi
+        list.Remove("Ketšup");
+
+        Console.WriteLine("\n--- Uuendatud koostisosad ---");
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Ülesanne 4 Otsing
+        Console.Write("\nSisesta koostisosa otsimiseks: ");
+        string otsitav = Console.ReadLine();
+
+        if (list.Contains(otsitav))
+            Console.WriteLine("Koostisosa on olemas!");
+        else
+            Console.WriteLine("Seda koostisosa meil retseptis ei ole.");
+
+        // Ülesanne 5 Salvestamine
+        try
+        {
+            File.WriteAllLines(path, list);
+            Console.WriteLine("\nUus retsept on edukalt faili salvestatud!");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Viga salvestamisel!");
+        }
+    }
+}
---
+++ StartPage_Osa4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class StartPage_Osa4
+{
+    static void Main(string[] args)
+    {
+        bool töötab = true;
+
+        while (töötab)
+        {
+            Console.WriteLine("Osa 4 Failitöötlus");
+            Console.WriteLine("1 - Salvesta lemmik Itaalia toit");
+            Console.WriteLine("2 - Kuva kogu menüü");
+            Console.WriteLine("3 - Koostisosade haldus (muutmine, otsing, salvestus)");
+            Console.WriteLine("0 - Välju");
+            Console.Write("Vali tegevus: ");
+
+            string valik = Console.ReadLine();
+
+            switch (valik)
+            {
+                case "1":
+                    Osa4_Funktsioonid.SalvestaLemmikToit();
+                    break;
+                case "2":
+                    Osa4_Funktsioonid.KuvaMenuu();
+                    break;
+                case "3":
+                    Osa4_Funktsioonid.KoostisosadeHaldus();
+                    break;
+                case "0":
+                    töötab = false;
+                    break;
+                default:
+                    Console.WriteLine("Vale valik!");
+                    break;
+            }
+        }
+    }
+}
